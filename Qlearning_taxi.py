243ca86b1917f8ab3c662d418a114aeb0902be36
Data Machinist <datamachinist@gmail.com>
2019-01-14 12:03:32 +0000

    removed lake environment

--- Qlearning_taxi.py
+++ Qlearning_taxi.py
@@ -3,71 +3,54 @@ import matplotlib.pyplot as plt
 import gym
 import random
 
-# create the environment
+# CREATE THE ENVIRONMENT
 env = gym.make("Taxi-v2")
-#env = gym.make("FrozenLake-v0")
-#env = gym.make("FrozenLake8x8-v0")
-env.render()
-
 action_size = env.action_space.n
-print("Action space size: ", action_size)
-
 state_size = env.observation_space.n
+print("Action space size: ", action_size)
 print("State space size: ", state_size)
 
-# initialise the Q table
-qtable = np.zeros((state_size, action_size))
-print(qtable)
+# INITIALISE Q TABLE TO ZERO
+Q = np.zeros((state_size, action_size))
 
-# hyperparameters for the taxi
-train_episodes = 50000        # Total episodes
+# HYPERPARAMETERS
+train_episodes = 2000         # Total train episodes
 test_episodes = 100           # Total test episodes
-max_steps = 99                # Max steps per episode
-learning_rate = 0.7           # Learning rate
+max_steps = 100               # Max steps per episode
+alpha = 0.7                   # Learning rate
 gamma = 0.618                 # Discounting rate
 
-# hyperparameters for the frozen lake
-#train_episodes = 15000        # Total episodes
-#test_episodes = 100           # Total test episodes
-#max_steps = 99                # Max steps per episode
-#learning_rate = 0.8           # Learning rate
-#gamma = 0.95                  # Discounting rate
-
-# Exploration parameters
+# EXPLORATION / EXPLOITATION PARAMETERS
 epsilon = 1                   # Exploration rate
 max_epsilon = 1               # Exploration probability at start
 min_epsilon = 0.01            # Minimum exploration probability 
 decay_rate = 0.01             # Exponential decay rate for exploration prob
 
-# implement the Q learning algorithm: training phase
+# TRAINING PHASE
 training_rewards = []   # list of rewards
 
 for episode in range(train_episodes):
-    # Reset the environment
-    state = env.reset()
-    step = 0
-    done = False
+    state = env.reset()    # Reset the environment
     cumulative_training_rewards = 0
     
     for step in range(max_steps):
-        # Choose an action a in the current world state (s)
-        exp_exp_tradeoff = random.uniform(0,1)   # choose a random number
+        # Choose an action (a) among the possible states (s)
+        exp_exp_tradeoff = random.uniform(0, 1)   # choose a random number
         
-        # If this number > epsilon --> exploitation (Select the action corresponding to the biggest Q value for this state)
+        # If this number > epsilon, select the action corresponding to the biggest Q value for this state (Exploitation)
         if exp_exp_tradeoff > epsilon:
-            action = np.argmax(qtable[state,:])        
-        # Else choose a random action --> exploration
+            action = np.argmax(Q[state,:])        
+        # Else choose a random action (Exploration)
         else:
             action = env.action_space.sample()
         
         # Perform the action (a) and observe the outcome state(s') and reward (r)
         new_state, reward, done, info = env.step(action)
 
-        # Update the Q table for that state and that action using the Bellman equation: Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
-        qtable[state, action] = qtable[state, action] + learning_rate * (reward + gamma * 
-                                    np.max(qtable[new_state, :]) - qtable[state, action]) 
+        # Update the Q table using the Bellman equation: Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
+        Q[state, action] = Q[state, action] + alpha * (reward + gamma * np.max(Q[new_state, :]) - Q[state, action]) 
         cumulative_training_rewards += reward  # increment the cumulative reward        
-        state = new_state # Update the state
+        state = new_state         # Update the state
         
         # If we reach the end of the episode
         if done == True:
@@ -81,30 +64,27 @@ for episode in range(train_episodes):
     training_rewards.append(cumulative_training_rewards)
 
 print ("Training score over time: " + str(sum(training_rewards)/train_episodes))
-print(qtable)
 
 x = range(train_episodes)
 plt.plot(x, training_rewards)
 plt.xlabel('episode')
 plt.ylabel('Training cumulative reward')
-plt.show()
+plt.savefig('Q-learning.png', dpi=300)
+# plt.show()
     
-# Test phase
-env.reset()
+# TEST PHASE
 test_rewards = []
 
 for episode in range(test_episodes):
     state = env.reset()
-    step = 0
-    done = False
     cumulative_test_rewards = 0
     print("****************************************************")
     print("EPISODE ", episode)
 
     for step in range(max_steps):
-#        env.render() # COMMENT IT IF YOU WANT TO SEE OUR AGENT PLAYING
+        env.render()             # UNCOMMENT IT IF YOU WANT TO SEE THE AGENT PLAYING
         # Take the action (index) that have the maximum expected future reward given that state
-        action = np.argmax(qtable[state,:])        
+        action = np.argmax(Q[state,:])        
         new_state, reward, done, info = env.step(action)
         cumulative_test_rewards += reward
         state = new_state
@@ -117,8 +97,4 @@ for episode in range(test_episodes):
 env.close()
 print ("Test score over time: " + str(sum(test_rewards)/test_episodes))
 
-x = range(test_episodes)
-plt.plot(x, test_rewards)
-plt.xlabel('episode')
-plt.ylabel('Testing cumulative reward')
-plt.show()
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Qlearning_taxi
 Implement Q learning for openAI's taxi environmnent
 
+Please refer to [this article](https://www.datamachinist.com/reinforcement-learning/part-2-q-learning-to-solve-the-taxi-problem/) for more information.
+
